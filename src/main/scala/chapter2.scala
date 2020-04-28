87e5da6491c806fe3a9bebe8bb4bf2ba2126253d
Darth Spectrum <muradianator@gmail.com>
2020-04-29 03:35:15 +0400

    Excercises 3, 4 and 5 from Chapter 2

--- src/main/scala/chapter2.scala
+++ src/main/scala/chapter2.scala
@@ -3,6 +3,8 @@ package chapter2
 
 object Chapter2 {
 
+  def partial[A, B, C](a: A, f: (A, B) => C): B => C = b => f(a, b)
+
   // Excercise 2.1
   def Fib(n: Int): Int = {
     @annotation.tailrec
@@ -27,4 +29,13 @@ object Chapter2 {
     if (as.isEmpty || as.length == 1) true
     else loop(0)
   }
+
+  // Excercise 2.3
+  def curry[A, B, C](f: (A, B) => C): A => B => C = a => partial(a, f)
+  
+  // Excercise 2.4
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = (a, b) => f(a)(b)
+
+  // Excercise 2.5
+  def compose[A, B, C](f: B => C, g: A => B): A => C = a => f(g(a))
 }
